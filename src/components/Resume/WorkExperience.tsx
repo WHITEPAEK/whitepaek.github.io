1a8f305aef6afb9efa773df346147845d3826bcd
SEUNGJOO PAEK <atomsjp@gmail.com>
2025-05-11 20:55:50 +0900

    feat(resume): 경력 내용 추가

--- src/components/Resume/WorkExperience.tsx
+++ src/components/Resume/WorkExperience.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ResumeSection from "@/components/Resume/ResumeSection.tsx";
 import ResumeGridItem from "@/components/Resume/ResumeGridItem.tsx";
+import Link from "@/components/Resume/Link.tsx";
+
+interface ResponsibilityItem {
+  text: string;
+  url?: string;
+}
 
 interface WorkExperience {
   period: string;
   duration: string;
   company: string;
   position: string;
-  responsibilities: string[];
+  responsibilities: ResponsibilityItem[];
 }
 
 const workExperiences: WorkExperience[] = [
@@ -16,14 +22,52 @@ const workExperiences: WorkExperience[] = [
     duration: "1년 4개월",
     company: "아사삭",
     position: "Backend engineer & Co-Founder",
-    responsibilities: ["..."],
+    responsibilities: [
+      {
+        text: `프레시센터, 오프라인 매장 오픈`,
+      },
+      {
+        text: `프로젝트 리더 역할 수행`,
+      },
+      {
+        text: `픽업 주문 서비스 "아사삭 스토어" 오픈 (매출 31.8% 증가)`,
+      },
+    ],
   },
   {
     period: "2020. 01 - 2023. 12",
     duration: "4년",
     company: "비디",
     position: "Backend engineer",
-    responsibilities: ["..."],
+    responsibilities: [
+      {
+        text: `SKT 에이닷 캐릭터, 모션, 아이템 오브젝트 관리를 위한 내부 시스템 "Adot Studio" 오픈`,
+        url: `https://news.sktelecom.com/tag/%EC%97%90%EC%9D%B4%EB%8B%B7/page/9`,
+      },
+      {
+        text: `SKT 이프랜드(MAU 437만) 아바타 코스튬/모션 제작 플랫폼 "ifland Studio" 오픈`,
+        url: `https://news.sktelecom.com/tag/ifland`,
+      },
+      {
+        text: `SKT 점프 AR 콘텐츠 관리 시스템 "Massive AR" 오픈`,
+        url: `https://news.sktelecom.com/tag/%EC%A0%90%ED%94%84ar`,
+      },
+      {
+        text: `회사 홈페이지 개발 및 Linux 기반 내부 서버 구축`,
+      },
+      {
+        text: `COVID-19 비대면 전환 대응을 위한 FortiGate 방화벽 도입 및 원격 근무 환경 구축`,
+      },
+      {
+        text: `본부 인턴/신입 개발자 멘토 역할 수행`,
+      },
+      {
+        text: `본부 개발파트 리더 역할 수행`,
+      },
+      {
+        text: `2023년 본부 추천으로 <특별승진>`,
+      },
+    ],
   },
 ];
 
@@ -47,7 +91,13 @@ const WorkExperience = () => {
               <p className="mb-2 text-gray-700">{exp.position}</p>
               <ul className="list-disc space-y-1 pl-5 text-sm leading-relaxed text-gray-600">
                 {exp.responsibilities.map((item, i) => (
-                  <li key={i}>{item}</li>
+                  <li key={i}>
+                    {item.url ? (
+                      <Link href={item.url}>{item.text}</Link>
+                    ) : (
+                      item.text
+                    )}
+                  </li>
                 ))}
               </ul>
             </>
