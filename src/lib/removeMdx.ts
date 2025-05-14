0b8a75786b3650d1bbab4a3a2f7e34d4ad46acc6
SEUNGJOO PAEK <atomsjp@gmail.com>
2025-05-15 05:47:25 +0900

    feat: 마크다운 코드 블록 제거 추가

--- src/lib/removeMdx.ts
+++ src/lib/removeMdx.ts
@@ -3,8 +3,9 @@ import removeMarkdown from "remove-markdown";
 export function removeMdx(raw: string): string {
   return removeMarkdown(
     raw
-      .replace(/^import .*?$/gm, "")
-      .replace(/<[^>]+>/g, "")
-      .replace(/\{.*?}/gs, ""),
+      .replace(/^import .*?$/gm, "") // import 제거
+      .replace(/<[^>]+>/g, "") // JSX 태그 제거
+      .replace(/\{.*?}/gs, "") // 중괄호 내 JS 제거
+      .replace(/```[\s\S]*?```/g, ""), // 코드 블록 제거
   ).trim();
 }
